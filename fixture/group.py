9b88e6eba98588c5ac7c3f9206fd57f9b7f41cfe
ZBaisakalova <cestsecret@mail.ru>
2019-12-25 00:52:10 +0300

    Сделать фикстуру общей для всех тестов ДЗ6

--- fixture/group.py
+++ fixture/group.py
@@ -6,11 +6,11 @@ class GroupHelper:
         self.app = app
 
     def return_to_groups_page(self):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
-        wd = self.wd
+        wd = self.app.wd
         self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
@@ -28,6 +28,15 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
     def open_groups_page(self):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
