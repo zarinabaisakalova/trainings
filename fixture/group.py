7d68fa4ae58cb6a151b2ba7e6b0b9b475accb77b
zarina_b <zarinabaisakalova@gmail.com>
2020-02-11 15:48:53 +0300

    Повышаем "интеллектуальность" тестов: обеспечение выполнения предусловий для удаления групп

--- fixture/group.py
+++ fixture/group.py
@@ -48,3 +48,10 @@ class GroupHelper:
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
+
+
