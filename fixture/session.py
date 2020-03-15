51e44826853f6d71c3687bf95e875ba3114d9e15
zarina_b <zarinabaisakalova@gmail.com>
2020-03-15 12:43:06 +0300

    Улучшение функции чтения информации о пользователе из тестируемого приложения

--- fixture/session.py
+++ fixture/session.py
@@ -14,7 +14,7 @@ class SessionHelper:
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         #wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+        wd.find_element_by_css_selector('input[type="submit"]').click()
 
     def logout(self):
         wd = self.app.wd
@@ -31,7 +31,11 @@ class SessionHelper:
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//div[@id='top']/form/b").text == "("+username+")"
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div[@id='top']/form/b").text[1:-1]
 
     def ensure_login(self, username, password):
         wd = self.app.wd
