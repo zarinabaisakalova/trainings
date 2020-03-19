e4c40a5ea2ac9db569e91bc686bd31afb92af777
zarina_b <zarinabaisakalova@gmail.com>
2020-03-20 01:13:15 +0300

    Задание №14: фикс

--- test/test_phones.py
+++ test/test_phones.py
@@ -11,8 +11,7 @@ def test_phones_on_home_page(app):
     assert contact_from_home_page.lastname == contact_info_from_edit_page.lastname
     assert contact_from_home_page.address == contact_info_from_edit_page.address
     assert contact_from_home_page.all_emails_from_home_page == merge_phones_like_on_home_page(contact_info_from_edit_page)
-
-def test_phones_on_contact_view_page(app):
+#def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_info_from_view_page(0)
     contact_info_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_view_page.homephone == contact_info_from_edit_page.homephone
