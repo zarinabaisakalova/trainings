fd6a7184b20eb7f704fb36ecb96ba66a7c4995fc
zarina_b <zarinabaisakalova@gmail.com>
2020-05-15 00:17:05 +0300

    Ещё один способ параметризации тестов: динамическая генерация тестов

--- test/conftest.py
+++ test/conftest.py
@@ -4,6 +4,7 @@ import pytest
 import json
 import os.path
 from fixture.application import Application
+import importlib
 
 fixture = None
 target = None
@@ -33,3 +34,13 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--target", action="store", default="target.json")
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_form_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_form_module(module):
+    return importlib.import_module("data.%s" % module).testdata
