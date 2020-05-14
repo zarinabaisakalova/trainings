7e9529b415fadddef2d0c92637e54847927a9c77
zarina_b <zarinabaisakalova@gmail.com>
2020-05-15 00:33:54 +0300

    Загрузка тестовых данных из файлов в формате JSON

--- test/conftest.py
+++ test/conftest.py
@@ -5,6 +5,7 @@ import json
 import os.path
 from fixture.application import Application
 import importlib
+import jsonpickle
 
 fixture = None
 target = None
@@ -38,9 +39,16 @@ def pytest_addoption(parser):
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
         if fixture.startswith("data_"):
-            testdata = load_form_module(fixture[5:])
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json"):
+            testdata = load_from_module (fixture[5:])
             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
 
 
-def load_form_module(module):
+def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
