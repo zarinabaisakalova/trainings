060f08a2eda778a70cf57b7d780e119973aa7b11
zarina_b <zarinabaisakalova@gmail.com>
2020-05-17 23:57:58 +0300

    ORM – Object Relational Mapping

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,12 +1,12 @@
 import pymysql.cursors
-from fixture.db import DbFixture
+from fixture.orm import ORMFixture
 
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
+    l = db.get_contact_list()
+    for item in l:
+        print(l)
+    print(len(l))
 finally:
-    db.destroy()
+    pass  # db.destroy()
