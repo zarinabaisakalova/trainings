e2d4cc546543fd971852ae0371c0fb0a49cb2dbe
zarina_b <zarinabaisakalova@gmail.com>
2020-05-17 20:41:08 +0300

    Фикстура для взаимодействия с базой данных

---
+++ fixture/db.py
@@ -0,0 +1,17 @@
+import pymysql.cursors
+from model.group import Group
+from model.contact import Contact
+
+
+class DbFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+
+
+    def destroy(self):
+        self.connection.close()
